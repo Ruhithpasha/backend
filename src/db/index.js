d98f9322f4315f4f1a594acd7ea360e7d386a4c5
Ruhithpasha <93814271+Ruhithpasha@users.noreply.github.com>
2024-11-10 18:55:23 +0530

    c

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connect_DB = async () => {
+
+
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`connection successfull to ${connectionInstance.connection.host}`);
+        console.log("Hello")
+
+    } catch (error) {
+        console.log("Error aya hai", error)
+        process.exit(1)
+    }
+}
+
+
+
+export default connect_DB;
---
+++ src/index.js
@@ -0,0 +1,40 @@
+// Initiate the connection to the database
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+// import connect_DB from "./db";
+
+
+
+// const app = express();
+
+// this is an beginner approach to connect to the database
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Ye le", error);
+//             throw error
+//         })
+
+//       app.listen(process.env.PORT),()=>{
+//         console.log(`app is running on port ${process.env.PORT}`)
+//       }
+         
+//     } catch (error) {
+//         console.log("Ye le ", error)
+//     }
+// })
+
+
+
+
+// this is an advanced approach to connect to the database
+
+import dotenv from 'dotenv';
+import connect_DB from './db/index.js';
+
+dotenv.config({
+    path : './env'
+})
+
