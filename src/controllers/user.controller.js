7fbc2a4043e32f2c2ff04f7191023a3a71857beb
Ruhithpasha <93814271+Ruhithpasha@users.noreply.github.com>
2024-11-15 09:41:06 +0530

    c

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,9 @@
 import asyncHandler from '../utils/asyncHandler.js';
 // below function is used to register a user
 // The registerUser function is created in the src/controllers/user.controller.js file. The registerUser function is an asynchronous function that takes the request and response objects as arguments. The registerUser function sends a response with a status code of 200 and a JSON object with a message property set to "ok".
+
+
+// this is a controller function to register a user
 const registerUser = asyncHandler(async (req,res)=>{
     res.status(200).json({
         message:"ok"
